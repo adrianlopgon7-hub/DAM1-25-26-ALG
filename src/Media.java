8a42bf513dfdbed021290410f7fbcf86c937c1d0
Adrian Lopez <adrianlopgon7@gmail.com>
2025-09-18 19:05:20 +0200

    Ejercicios

---
+++ src/Media.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Media  {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int num3;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Dime cual es la primera nota:");
+        num1 = sc.nextInt();
+
+        System.out.print("Dime cual es la segunda nota:");
+        num2 = sc.nextInt();
+
+        System.out.print("Dime cual es la tecera nota:");
+        num3 = sc.nextInt();
+
+        double total = (num1 + num2 + num3) /3.05;
+        sc.close();
+
+        System.out.println("tu media es de:" + total);
+        
+    
+    }
+}

29b9a0acaa72b37e81df1555c85941c68d49a078
Adrián <adrianlpogon7@gmail.com>
2025-09-26 13:39:14 +0200

    Tareas clase dia 26

---
+++ src/E0109_Par.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class E0109_Par {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.print("Escribe aqui un númeor entero");
+            
+            int numero = 8;
+            boolean esPar = (numero % 2 == 0);
+
+            System.out.println("¿El numero " + numero + "es par?" + es par);
+
+    }
+}
